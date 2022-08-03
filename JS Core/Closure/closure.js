11847bb779e203b5cb8cd4affcd7c0e365590e89
HoaDoan <Dinh.Anh@intertecsys.com>
2022-08-03 16:23:58 +0700

    closure

---
+++ JS Core/Closure/closure.js
@@ -0,0 +1,14 @@
+/** @format */
+
+function a(number) {
+  debugger;
+  const a1 = 10;
+  function b(number2) {
+    return c;
+  }
+  c = a1 + number;
+  return b();
+}
+
+let d = a(20);
+console.log(d);
